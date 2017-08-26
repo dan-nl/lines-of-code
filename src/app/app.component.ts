972466aa426d1ce4038f502730df04a9a72fffc3
dan-nl <d_entous@yahoo.com>
2017-08-26 08:07:23 +0200

    added initial component logic and hooked it up to the html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  lines_of_code = 0;
+
+  /**
+   * @param {String} code
+   *
+   * @returns {Number}
+   */
+  public static processCode(code) {
+    let result = 0;
+
+    if (typeof code !== 'string') {
+      console.warn('AppComponent.processCode(): expects a String, but got a [' + typeof code + ']');
+      return result;
+    }
+
+    let code_lines = code.split('\n');
+
+    return code_lines.reduce(
+      (acc) => {
+        return acc + 1;
+      },
+      result
+    );
+  }
+
+  /**
+   * @param {Event} event
+   */
+  onKey(event: any) {
+    this.lines_of_code = AppComponent.processCode(event.target.value);
+  }
 }
